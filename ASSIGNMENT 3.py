95952b3e8f78b3c9ae765212b97baf9f33e81060
Nishant-01-py <nishantchourawar2025@gmail.com>
2025-07-28 19:38:57 +0530

    Add files via upload

---
+++ ASSIGNMENT 3.py
@@ -0,0 +1,17 @@
+#Task 1: Calculate Factorial Using a Function
+num = int (input("Enter a number: "))
+def factorial(num):
+    if (num<2):
+        return 1
+    else:
+        return (num*(factorial(num-1)))
+result = factorial(num)
+print(f"Factorial of {num} is:",result)
+
+
+#Task 2: Using the Math Module for Calculations
+a = int (input("Enter a number: "))
+from math import *
+print("Square root: ",sqrt(a))
+print("Logarithm: ",log(a))
+print("Sine: ",sin(a))
